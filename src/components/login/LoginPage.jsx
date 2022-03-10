224e32745b4773104d9f5269861ba2f0f00ff683
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-03-09 23:21:29 -0600

    logout & login

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export const LoginPage = () => {
+  
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate('/', {
+      replace: true
+    });
+  }
+  
   return (
-    <div>login page</div>
+    <div className="container mt-5">
+      <h1>login</h1>
+      <hr />
+      <button
+        onClick={ handleLogin }
+        className="btn btn-primary"
+      >
+        login
+      </button>
+    </div>
   )
 }
