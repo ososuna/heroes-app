10635c1103d83658ed399fba7c72c580a827ddd5
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-03-11 17:32:13 -0600

    hero card component

--- src/components/hero/HeroList.jsx
+++ src/components/hero/HeroList.jsx
@@ -1,3 +1,5 @@
+
+import { HeroCard } from "./HeroCard";
 import { getHeroesByPublisher } from "../../helpers/getHeroesByPublisher"
 
 export const HeroList = ({ publisher }) => {
@@ -5,16 +7,12 @@ export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher( publisher );
   
   return (
-    <>
-      <ul>
-        {
-          heroes.map( hero => (
-            <li key={ hero.id }>
-              { hero.superhero.toLowerCase() }
-            </li>
-          ))
-        }
-      </ul>
-    </>
+    <div className="row row-cols-1 row-cols-md-3 g-3">
+      {
+        heroes.map( hero => (
+          <HeroCard key={ hero.id } { ...hero } />
+        ))
+      }
+    </div>
   )
 }
