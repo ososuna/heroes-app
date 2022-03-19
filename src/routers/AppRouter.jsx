4d09c0dca1d160e8f5f1c811822f361c70943d7b
oswi <oswaldo.adrian@live.com.mx>
2022-03-19 16:04:47 -0600

    public & private routes

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import { DashboardRoutes } from './DashboardRoutes'
+import { PrivateRoute } from './PrivateRoute'
 import { LoginPage } from '../components/login/LoginPage'
+import { PublicRoute } from './PublicRoute'
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={ <LoginPage /> } />
-        <Route path="/*" element={ <DashboardRoutes /> } />
+        <Route path="/login" element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+        <Route path="/*" element={
+            <PrivateRoute>
+              <DashboardRoutes />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
